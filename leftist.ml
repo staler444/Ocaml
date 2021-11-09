32733e65631cc21462c6ea445e6521a188556a8e
staler444 <staler444@bartek-blaszak.localdomain>
2021-11-09 17:37:54 +0100

    komentarze są

--- leftist.ml
+++ leftist.ml
@@ -1,11 +1,23 @@
+(*
+definicja losowa: długość ścieżki zerowej (npl).
+zerowa długość ścieżki (npl) 
+węzła to liczba węzłów między nim a wartością null w drzewie
+*)
+
 type 'a queue = | Node of 'a queue * 'a queue * 'a * int | Null;;
+(* (lewe poddrzewo, prawe poddrzewo, wartosc węzła, wartosc npl) albo null*)
 
 exception Empty;; 
+(* wyjątek empty podnoszony kiedy chcemy usunąć coś z pustej kolejki*)
 let empty = Null;;
+(*pusta kolejka*)
+
 let make_queue a = Node(Null, Null, a, 0);;
+
 let get_npl a = match a with 
-    | Null -> 0
+    | Null -> (-1)
     | Node (_, _, _, npl) -> npl;;
+(*zwraca npl wierzchołka*)
 
 let rec join a b = match a, b with 
     | (Null, tr) | (tr, Null) -> tr
@@ -14,8 +26,10 @@ let rec join a b = match a, b with
         then Node(l1, new_r, val1, (get_npl new_r)+1)
         else Node(new_r, l1, val1, (get_npl l1)+1)
     else (join b a);;
+(*merguje 2 kolejki w jedną*)
 
 let add b q = join q (make_queue b);;
+
 let delete_min q = match q with
     | Node(l, r, el, npl) -> (el, (join l r))
     | Null -> raise Empty;;
